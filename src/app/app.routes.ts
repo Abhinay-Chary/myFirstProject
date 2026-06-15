8cb16fdd021c1604e8faf26d907c3f0f4d0ecbbd
AbhinayChary <abhinaychary1@gmail.com>
2026-06-15 14:43:37 +0530

    update in appconfig

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,7 +2,6 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
 import { AddFruitComponent } from './add-fruit/add-fruit.component';
-import { authGuard } from './auth.guard';
 import { RxjsTestComponent } from './rxjs-test/rxjs-test.component';
 
 export const routes: Routes = [
